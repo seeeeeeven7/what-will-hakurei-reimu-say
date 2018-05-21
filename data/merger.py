873e402583aa0d802cd4f98e5b9931cb6a1f829f
Steven Hu <SourceDark@gmail.com>
2018-05-21 20:38:59 +0800

    fix dataset

--- data/merger.py
+++ data/merger.py
@@ -9,4 +9,20 @@ with open('talk_in_game/all.txt', 'w', encoding = 'utf8') as outfile:
                 continue
             lines = open('talk_in_game' + '/' + filename, 'r', encoding = 'utf8').read().splitlines()
             for line in lines:
-                print(' '.join(list(line)), file = outfile)
+                h = 0
+                f = False
+                s = ''
+                for ch in list(line):
+                    if ch == '<':
+                        h += 1
+                    else:
+                        if ch == '>':
+                            h -= 1
+                        else:
+                            if h == 0:
+                                if f:
+                                    s += ' '
+                                else:
+                                    f = True
+                                s += ch
+                print(s, file = outfile)
